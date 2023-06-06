f21649f898d9d2daf7acb3e3c776c3c63a325fec
Ankit Sharma <sharmaa7861@gmail.com>
2023-06-06 19:34:26 +0530

    updating changes

--- app/src/main/java/com/example/pairperfect/SplashScreen.kt
+++ app/src/main/java/com/example/pairperfect/SplashScreen.kt
@@ -2,10 +2,19 @@ package com.example.pairperfect
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
 
 class SplashScreen : AppCompatActivity() {
+    private lateinit var rvBoard: RecyclerView
+    private lateinit var tvNumMoves: TextView
+    private lateinit var tvNumPairs: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        rvBoard = findViewById(R.id.rvBoard)
+        tvNumMoves = findViewById(R.id.tvNumMoves)
+        tvNumPairs = findViewById(R.id.tvNumPairs)
     }
 }
